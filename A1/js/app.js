c8ba88275f9e534f43fe1118771aa218350ab37f
R3becka <rebhel464@ihmeducation.se>
2024-09-26 11:13:29 +0200

    Updated

--- A1/js/app.js
+++ A1/js/app.js
@@ -8,59 +8,63 @@ let aiRollText = document.getElementById("aiRollText");
 let evaluationText = document.getElementById("evaluationText");
 let playerScoreText = document.getElementById("playerScoreText");
 let aiScoreText = document.getElementById("aiScoreText");
-
-
-
+let drawResultText = document.getElementById("drawResultText");
 //DATA
-
  let playerRoll = 0;
 let aiRoll = 0;
 let playerScore = 0;
 let aiScore = 0;
+let drawResult =0;
 
 
 //Process
-button0.addEventListener("click", ()=> {
+button0.addEventListener("click", function() {
   getRandomNumberPlayer();
   getRandomNumberAi();
   showPlayerRollResult();
   showAiRollResult();
   evaluateRoll();
   updateScores();
-
 })
 
 //CONTROLLER
 function getRandomNumberPlayer() {
-  playerRoll = Math.floor(Math.random() * 6) + 1 + " player rolled"
-
+  playerRoll = Math.floor(Math.random() * 6) + 1
 }
+
 function getRandomNumberAi() {
   aiRoll = Math.floor(Math.random() * 6) + 1
 }
+
 function evaluateRoll() {
   if (playerRoll > aiRoll) {
-    evaluationText.innerHTML= "You Win";
+    evaluationText.innerHTML= "<span style='color: green;'>You Win!</span>";
     playerScore ++;
   }
   else if (playerRoll < aiRoll) {
-    evaluationText.innerHTML = "Ai Wins";
+    evaluationText.innerHTML = "<span style='color: red;'>Ai Wins</span>";
     aiScore++;
   }
-  else {
-    evaluationText.innerHTML = "It's a draw";
+  else if (playerRoll === aiRoll) {
+    evaluationText.innerHTML = "<span style='color: yellow;'>It's a draw</span>";
+drawResult++;
   }
+}
 
-
-
-  }
 function showPlayerRollResult() {
-  playerRollText.innerHTML = playerRoll;
+  playerRollText.innerHTML = "Player rolled " + playerRoll;
 }
+
 function showAiRollResult() {
- aiRollText.innerHTML = aiRoll;
+ aiRollText.innerHTML = "AI rolled: " + aiRoll;
 }
+
 function updateScores() {
-  playerScoreText.innerHTML = playerScore;
-  aiScoreText.innerHTML = aiScore;
+  playerScoreText.innerHTML = "Player total wins: " + playerScore.toString();
+  aiScoreText.innerHTML = "AI total wins. " + aiScore.toString();
+  drawResultText.innerHTML = "Total Draws: " + drawResult;
 }
+
+document.cookie = "todaysDay=Thursday: expires=Thu, 29 Sep 2024 00:00;00 GMT";
+
+
